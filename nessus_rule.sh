c2e460b6d803c13cf199cc9f7446b6436b022f98
root <justinltodd@gmail.com>
2016-04-25 11:56:02 -0500

    Updated Help information request.

--- nessus_rule.sh
+++ nessus_rule.sh
@@ -35,12 +35,12 @@ fi
 # GET OPTIONS FROM COMMAND LINE [-h] help [-a] accept [-r] reject [-n] plugin-reject [-k] plugin-accept
 while getopts ":h:a:r:n:k:p:" opt; do
   case $opt in
-    h)
-      echo "-h was triggered: $OPTARG" >&2
+    h | *)
+      echo ""
       echo "Syntax : accept|reject address/netmask ie. reject 10.42.123.0/24 . 10.42.123.1/24"
       echo "Reject connecting to port 80 for 10.0.0.1 ie. reject 10.0.0.1:80"
       echo "Forbid connecting to ports 8000 - 10000 ie. reject 192.168.0.0/24:8000-10000"
-      echo "You can also deny/allow the use of certain plugin IDs ie. plugin-reject 10335 or plugin-accept 10000-40000"
+      echo "You can also deny|allow the use of certain plugin IDs ie. plugin-reject 10335 or plugin-accept 10000-40000"
       echo ""
       echo "Syntax: [-h] help [-a] accept [-r] reject [-n] plugin-reject [-k] plugin-accept"
       echo "[-a] [IP/CIDR]"
@@ -48,6 +48,7 @@ while getopts ":h:a:r:n:k:p:" opt; do
       echo "[-p] [IP/CIDR:PORT] [IP/CIDR:PORT-PORT] " 
       echo "[-n] [plugin ID]"
       echo "[-k] [plugin ID]"
+      echo ""
       ;;
     a)
       # CHECK FOR IMPROPER RULE SYNTAX CHAR ":"
